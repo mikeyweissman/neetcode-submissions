752118d438c4c6f5e0f029d4e97345997ebeea0e
Mikey Weissman <97177791+mikeyweissman@users.noreply.github.com>
2026-09-11 14:46:05 +0300

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        s1count = {}
+
+        for i in s1:
+            s1count[i] = s1count.get(i,0) + 1
+        
+
+        s2count = {}
+        l = 0
+
+        for r,rvalue in enumerate(s2):
+
+            if rvalue not in s1:
+                l = r+1
+                s2count = {}
+                continue
+            
+            s2count[rvalue] = s2count.get(rvalue,0) +1
+            
+            while s2count[rvalue] > s1count[rvalue]:
+                s2count[s2[l]] -= 1
+                l += 1
+            
+
+            if s1count == s2count:
+                return True
+        
+        return False
+            
+            
+            
